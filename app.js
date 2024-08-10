120d711661586c67872da383c55107bf0ef6e7f6
AhmadjonMurodullayev <murodullayevahmadjon26@gmail.com>
2024-08-10 15:47:34 +0500

    js

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const result = document.getElementById("result")
 const save = document.getElementById("save")
 let form = {}
 let edit_user = -1
+let current_page = 1;
+let items_per_page = 2;
 const users = JSON.parse(localStorage.getItem("users")) || []
 let search = ""
 document.addEventListener("DOMContentLoaded",function(){
@@ -38,14 +40,19 @@ function addUser(){
 
 }
 function displayUsers(){
-  result.innerHTML = ""
-  users.filter(item =>{
+  result.innerHTML = "";
+  let filtered_users = users.filter(item=>{
     if(item.first_name.toLowerCase().includes(search)){
-      return item
+        return item
     }
-  }).forEach((item,index) =>{
-    let tr = document.createElement("tr")
-    tr.innerHTML = `
+  })
+  let start_index = (current_page - 1) * items_per_page
+  let end_index = start_index + items_per_page
+  let display_users = filtered_users.slice(start_index,end_index)
+ 
+  display_users.forEach((item, index) => {
+      let tr = document.createElement("tr");
+      tr.innerHTML = `
     <td>${index + 1}</td>
     <td>${item.first_name}</td>
     <td>${item.last_name}</td>
@@ -55,13 +62,12 @@ function displayUsers(){
     <td>
 <button class="btn btn-info" onclick="editUser(${index})"> <i class="fa-solid fa-pen"></i></button>
 <button class="btn btn-info" onclick="deletUser(${index})"> <i class="fa-solid fa-trash"></i></button>
-
-
     </td>
-
     `
     result.appendChild(tr)
   })
+  paginationUsers(filtered_users.length)
+
 }
 function deletUser(index){
   users.splice(index, 1);
@@ -93,6 +99,25 @@ function toggleModal(status){
 }
 function saveStroge(){
   localStorage.setItem("users", JSON.stringify(users))
+}
+
+function paginationUsers(total_users){
+  let pagination_controls = document.getElementById("pagination-controls")
+  let total_pages = Math.ceil(total_users / items_per_page)
+  pagination_controls.innerHTML = ""
+  for(let i = 1; i<= total_pages; i++){
+      let page_btn = document.createElement("button")
+      page_btn.innerText = i
+      page_btn.className = i === current_page ? "btn btn-primary mx-1" : "btn btn-outline-primary mx-1"
+      page_btn.addEventListener("click", function(){
+          current_page = i
+          displayUsers()
+      })
+      pagination_controls.appendChild(page_btn)
+      saveStroge()
+      
+  }
+
 }
 
 
